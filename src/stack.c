68120010edebea0159282c250e07648a12015192
Jaehyun Lee <jaehylee@c1r24s1.42gyeongsan.kr>
2024-11-25 08:05:43 +0900

    impl rotate + update ft_printf

--- src/stack.c
+++ src/stack.c
@@ -1,31 +1,38 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.h                                        :+:      :+:    :+:   */
+/*   stack.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jaehylee <jaehylee@student.42gyeongsan.kr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/11/24 18:29:44 by jaehylee          #+#    #+#             */
-/*   Updated: 2024/11/25 06:45:40 by jaehylee         ###   ########.fr       */
+/*   Created: 2024/11/25 06:46:02 by jaehylee          #+#    #+#             */
+/*   Updated: 2024/11/25 07:00:19 by jaehylee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PUSH_SWAP_H
-# define PUSH_SWAP_H
-# include <unistd.h>
-# include <stdlib.h>
-# include "../ft_printf/src/ft_printf.h"
-
-typedef struct s_vec
-{
-	int		*ptr;
-	size_t	top;
-	size_t	len;
-	size_t	cap;
-}	t_vec;
+#include "push_swap.h"
 
 void	push(t_vec *vec, int value);
 int		*pop(t_vec *vec);
-void	rotate(t_vec *vec, int n);
 
-#endif
+void	rotate(t_vec *vec, int n)
+{
+	if (n == 0 || vec->len <= 1)
+		return ;
+	else if (n > 0)
+	{
+		if (vec->top == vec->len - 1)
+			vec->top = 0;
+		else
+			vec->top++;
+		rotate(vec, n - 1);
+	}
+	else
+	{
+		if (vec->top == 0)
+			vec->top = vec->len - 1;
+		else
+			vec->top--;
+		rotate(vec, n + 1);
+	}
+}
