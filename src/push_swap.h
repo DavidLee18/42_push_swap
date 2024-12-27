a0a4ca19f765ce7f3c31cd7cbf80855089a2bb2d
Jaehyun Lee <jaehylee@c1r24s1.42gyeongsan.kr>
2024-12-28 07:48:50 +0900

    impl measure_dist + analyze_stack

--- src/push_swap.h
+++ src/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: jaehylee <jaehylee@student.42gyeongsan.kr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 18:29:44 by jaehylee          #+#    #+#             */
-/*   Updated: 2024/12/28 00:36:22 by jaehylee         ###   ########.fr       */
+/*   Updated: 2024/12/28 04:55:28 by jaehylee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,6 +54,7 @@ size_t		hlen(t_min_heap h);
 
 void		analyze_stack(t_vec v, t_min_heap *h);
 void		print_cmds(t_vec v, t_min_heap *h);
+ssize_t		measure_dist(t_vec v, size_t i);
 
 size_t		min_usize(size_t a, size_t b);
 size_t		abs_isize(ssize_t i);
--- src/sort.c
+++ src/sort.c
@@ -6,7 +6,7 @@
 /*   By: jaehylee <jaehylee@student.42gyeongsan.kr> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 23:25:40 by jaehylee          #+#    #+#             */
-/*   Updated: 2024/12/28 01:35:46 by jaehylee         ###   ########.fr       */
+/*   Updated: 2024/12/28 07:46:48 by jaehylee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,34 @@ void	analyze_stack(t_vec v, t_min_heap *h)
 	while (i != v.bottom)
 	{
 		if (v.ptr[i] > v.ptr[wrapping_sub(i, 1, v.len - 1)])
-		{
-			if (i > v.len / 2)
-				insert(h, (ssize_t)i - (ssize_t)v.len);
-			else
-				insert(h, (ssize_t)i);
-		}
+			h->cap = insert(h, measure_dist(v, i));
 		i = wrapping_sub(i, 1, v.len - 1);
 	}
 }
 
 void	print_cmds(t_vec v, t_min_heap *h);
+
+ssize_t	measure_dist(t_vec v, size_t i)
+{
+	ssize_t	res1;
+	ssize_t	res2;
+	size_t	j;
+
+	j = i;
+	res1 = 0;
+	while (j != v.top)
+	{
+		j = wrapping_add(j, 1, v.len - 1);
+		res1++;
+	}
+	j = i;
+	res2 = 0;
+	while (j != v.top)
+	{
+		j = wrapping_sub(j, 1, v.len - 1);
+		res2--;
+	}
+	if (abs_isize(res1) <= abs_isize(res2))
+		return (res1);
+	return (res2);
+}
