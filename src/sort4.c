6581767336620c91e550e678490b75237de00b2e
DavidLee18 <djwodus@gmail.com>
2025-03-09 11:24:43 +0900

    brute force 4: raw but literally

--- src/sort4.c
+++ src/sort4.c
@@ -30,14 +30,48 @@ void	cmd_brute_4(const t_stack_pair *ss)
 		return ;
 	if (ss->a.ptr[0] == 3 && ss->a.ptr[1] == 4 && ss->a.ptr[2] == 2 && ss->a.ptr[3] == 1)
 		ft_printf("ra\nra\nsa\nra\nra\n");
-	if (ss->a.ptr[0] == 4 && ss->a.ptr[1] == 2 && ss->a.ptr[2] == 3 && ss->a.ptr[3] == 1)
+	else if (ss->a.ptr[0] == 4 && ss->a.ptr[1] == 2 && ss->a.ptr[2] == 3 && ss->a.ptr[3] == 1)
 		ft_printf("ra\nsa\nrra\n");
-	if (ss->a.ptr[0] == 2 && ss->a.ptr[1] == 4 && ss->a.ptr[2] == 3 && ss->a.ptr[3] == 1)
+	else if (ss->a.ptr[0] == 2 && ss->a.ptr[1] == 4 && ss->a.ptr[2] == 3 && ss->a.ptr[3] == 1)
 		ft_printf("rra\nsa\n");
-	if (ss->a.ptr[0] == 3 && ss->a.ptr[1] == 2 && ss->a.ptr[2] == 4 && ss->a.ptr[3] == 1)
+	else if (ss->a.ptr[0] == 3 && ss->a.ptr[1] == 2 && ss->a.ptr[2] == 4 && ss->a.ptr[3] == 1)
 		ft_printf("sa\nra\n");
-	if (ss->a.ptr[0] == 2 && ss->a.ptr[1] == 3 && ss->a.ptr[2] == 4 && ss->a.ptr[3] == 1)
+	else if (ss->a.ptr[0] == 2 && ss->a.ptr[1] == 3 && ss->a.ptr[2] == 4 && ss->a.ptr[3] == 1)
 		ft_printf("ra\nsa\nrra\nrra\nsa\n");
-	if (ss->a.ptr[0] == 4 && ss->a.ptr[1] == 3 && ss->a.ptr[2] == 1 && ss->a.ptr[3] == 2)
+	else if (ss->a.ptr[0] == 4 && ss->a.ptr[1] == 3 && ss->a.ptr[2] == 1 && ss->a.ptr[3] == 2)
 		ft_printf("sa\n");
+	else if (ss->a.ptr[0] == 3 && ss->a.ptr[1] == 4 && ss->a.ptr[2] == 1 && ss->a.ptr[3] == 2)
+		ft_printf("pb\nra\nsa\npa\nrra\n");
+	else if (ss->a.ptr[0] == 4 && ss->a.ptr[1] == 1 && ss->a.ptr[2] == 3 && ss->a.ptr[3] == 2)
+		ft_printf("rra\nrra\nsa\nra\n");
+	else if (ss->a.ptr[0] == 1 && ss->a.ptr[1] == 4 && ss->a.ptr[2] == 3 && ss->a.ptr[3] == 2)
+		ft_printf("rra\n");
+	else if (ss->a.ptr[0] == 3 && ss->a.ptr[1] == 1 && ss->a.ptr[2] == 4 && ss->a.ptr[3] == 2)
+		ft_printf("rra\nsa\nrra\n");
+	else if (ss->a.ptr[0] == 1 && ss->a.ptr[1] == 3 && ss->a.ptr[2] == 4 && ss->a.ptr[3] == 2)
+		ft_printf("ra\nsa\nrra\nrra\n");
+	else if (ss->a.ptr[0] == 4 && ss->a.ptr[1] == 2 && ss->a.ptr[2] == 1 && ss->a.ptr[3] == 3)
+		ft_printf("rra\nsa\nra\nra\n");
+	else if (ss->a.ptr[0] == 2 && ss->a.ptr[1] == 4 && ss->a.ptr[2] == 1 && ss->a.ptr[3] == 3)
+		ft_printf("pb\nsa\npa\nra\nra\n");
+	else if (ss->a.ptr[0] == 4 && ss->a.ptr[1] == 1 && ss->a.ptr[2] == 2 && ss->a.ptr[3] == 3)
+		ft_printf("rra\nsa\nrra\nrra\nsa\n");
+	else if (ss->a.ptr[0] == 1 && ss->a.ptr[1] == 4 && ss->a.ptr[2] == 2 && ss->a.ptr[3] == 3)
+		ft_printf("sa\nrra\n");
+	else if (ss->a.ptr[0] == 2 && ss->a.ptr[1] == 1 && ss->a.ptr[2] == 4 && ss->a.ptr[3] == 3)
+		ft_printf("ra\nra\n");
+	else if (ss->a.ptr[0] == 1 && ss->a.ptr[1] == 2 && ss->a.ptr[2] == 4 && ss->a.ptr[3] == 3)
+		ft_printf("ra\nra\nsa\n");
+	else if (ss->a.ptr[0] == 3 && ss->a.ptr[1] == 2 && ss->a.ptr[2] == 1 && ss->a.ptr[3] == 4)
+		ft_printf("ra\n");
+	else if (ss->a.ptr[0] == 2 && ss->a.ptr[1] == 3 && ss->a.ptr[2] == 1 && ss->a.ptr[3] == 4)
+		ft_printf("ra\nra\nsa\nrra\n");
+	else if (ss->a.ptr[0] == 3 && ss->a.ptr[1] == 1 && ss->a.ptr[2] == 2 && ss->a.ptr[3] == 4)
+		ft_printf("ra\nsa\n");
+	else if (ss->a.ptr[0] == 1 && ss->a.ptr[1] == 3 && ss->a.ptr[2] == 2 && ss->a.ptr[3] == 4)
+		ft_printf("rra\nsa\nra\n");
+	else if (ss->a.ptr[0] == 2 && ss->a.ptr[1] == 1 && ss->a.ptr[2] == 3 && ss->a.ptr[3] == 4)
+		ft_printf("sa\nra\nra\n");
+	else if (ss->a.ptr[0] == 1 && ss->a.ptr[1] == 2 && ss->a.ptr[2] == 3 && ss->a.ptr[3] == 4)
+		ft_printf("sa\nra\nra\nsa\n");
 }
